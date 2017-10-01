b710746944ad79c0870ebabeaa030428071092f7
joemarke <joe.ik.marke@gmail.com>
2017-10-01 13:40:03 +0100

    links working

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'BrunchBox' });
 });
 
+router.get('/music', function(req, res, next) {
+    res.render('music', { title: 'BrunchBox' });
+});
+
+router.get('/photos', function(req, res, next) {
+  res.render('photos', { title: 'BrunchBox' });
+});
+
+router.get('/videos', function(req, res, next) {
+    res.render('videos', { title: 'BrunchBox' });
+});
+
+router.get('/store', function(req, res, next) {
+    res.render('store', { title: 'BrunchBox' });
+});
+
 module.exports = router;
